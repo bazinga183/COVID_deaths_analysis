35c5caef68a695e32dda144f244c270d235cd2a7
Raquel Gaucin <>
2021-10-28 22:05:08 -0500

    minor edit to column data type

--- covid_cases_schema.sql
+++ covid_cases_schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE "covid_cases" (
     "id" int NOT NULL,
     "sex" varchar(6) NOT NULL,
     "age_group" varchar(20) NOT NULL,
-    "race_ethnicity" varchar(40) NOT NULL,
+    "race_ethnicity" varchar(80) NOT NULL,
     "hospital" varchar(7) NOT NULL,
     "med_cond" varchar(7) NOT NULL,
     "death" int NOT NULL,
