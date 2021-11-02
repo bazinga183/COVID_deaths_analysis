67930f6de5f9f870309eada67d3b56833d4c38b9
Raquel Gaucin <>
2021-11-02 14:09:17 -0500

    minor edit to column character limit

--- covid_cases_schema.sql
+++ covid_cases_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "covid_cases" (
     "id" int NOT NULL,
-    "sex" varchar(6) NOT NULL,
+    "sex" varchar(7) NOT NULL,
     "age_group" varchar(20) NOT NULL,
     "race_ethnicity" varchar(80) NOT NULL,
     "hospital" varchar(7) NOT NULL,
